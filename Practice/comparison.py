482233d5a198616a0a4226c19c561e39d11df689
Fredrik <fredrik.hasselgren02@gmail.com>
2025-02-02 09:11:11 +0000

    comparing pyquest/quimb circuit to quimb tebd

--- Practice/comparison.py
+++ Practice/comparison.py
@@ -3,11 +3,13 @@ import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
 import quimb.tensor as qtn
-from quimb.gen.operators import pauli
+from quimb.gen.operators import pauli, cX, Rz
 from scipy.linalg import logm
 
-def trotter_step(n: int, reg: Register, dt: float, J: float, h: float, factor: float =2, periodic: bool = True):
-    """ 1 trottized step performed on the given register """
+def trotter_step(n: int, obj, dt: float, J: float, h: float, r: int, factor: float =2, periodic: bool = True, getHam: bool = False):
+    """ 1 trottized step performed with PyQuest and Quimb """
+    reg = obj[0]  # For PyQuest
+    circ = obj[1] # For Quimb
 
     # Trotterized state
     U_trotter = np.eye(2**n, dtype=complex)
@@ -20,43 +22,60 @@ def trotter_step(n: int, reg: Register, dt: float, J: float, h: float, factor: f
         # Apply the interaction between qubit k and k+1
         X = unitaries.X(k, controls=k + 1)
         reg.apply_operator(X)
-        U_trotter = X.as_matrix(n) @ U_trotter
+        #circ.apply_gate('CX', k, k + 1, gate_round=r)
 
         # Apply the Rz rotation on qubit k
         Rz = unitaries.Rz(k, interaction_angle)
         reg.apply_operator(Rz)
-        U_trotter = Rz.as_matrix(n) @ U_trotter
+        #circ.apply_gate('RZ', interaction_angle, k, gate_round=r)        
 
         # Apply the interaction again
         reg.apply_operator(X)
-        U_trotter = X.as_matrix(n) @ U_trotter
+        #circ.apply_gate('CX', k, k + 1, gate_round=r)
+
+        circ.apply_gate('RZZ', interaction_angle, k, k+1)
+
+        if(getHam): # Generating efective Hamiltonian
+            U_trotter = X.as_matrix(n) @ U_trotter
+            U_trotter = Rz.as_matrix(n) @ U_trotter
+            U_trotter = X.as_matrix(n) @ U_trotter
 
     if periodic:
         #Periodic boundary condition
         X = unitaries.X(n-1, controls=0)
         reg.apply_operator(X)
-        U_trotter = X.as_matrix(n) @ U_trotter
         Rz = unitaries.Rz(n-1, interaction_angle)
-        U_trotter = Rz.as_matrix(n) @ U_trotter
         reg.apply_operator(Rz)
         reg.apply_operator(X)
-        U_trotter = Rz.as_matrix(n) @ U_trotter
+
+        #circ.apply_gate('CX', n - 1, 0, gate_round=r)
+        #circ.apply_gate('RZ', interaction_angle, n - 1, gate_round=r)
+        #circ.apply_gate('CX', n - 1, 0, gate_round=r)
+        circ.apply_gate('RZZ', interaction_angle, n-1, 0)
+
+        if(getHam):
+            U_trotter = X.as_matrix(n) @ U_trotter
+            U_trotter = Rz.as_matrix(n) @ U_trotter
+            U_trotter = Rz.as_matrix(n) @ U_trotter
 
     # Magnetic field
     for k in range(n):
         Rx = unitaries.Rx([k], self_angle)
         reg.apply_operator(Rx)
-        U_trotter = Rx.as_matrix(n) @ U_trotter
+        circ.apply_gate('RX', self_angle, k, gate_round=r)        
+
+        if(getHam):
+            U_trotter = Rx.as_matrix(n) @ U_trotter
 
     # Calculating the effective Hamiltonian
     H_eff = 1j / dt * logm(U_trotter)
     
     # eigvals_trotter, eigvecs_trotter
-    return np.linalg.eigh(H_eff)
+    return np.linalg.eigh(H_eff) # Identity if getHam = False
 
 # Variables
 n = 6  # Number of qubits
-N = 1000  # Number of timesteps
+N = 200  # Number of timesteps
 T = 10  # Total time
 dt = T / N  # Timestep size
 J = 0.2  # Interaction strength
@@ -64,8 +83,9 @@ h = 1.2  # Magnetic field coupling
 target = 0
 alpha = np.pi / 8  # Magnetic field angle of incidence
 
-# Initialize the quantum register
+# Initialize the quantum registers
 reg = Register(n)
+circ = qtn.Circuit(n)
 
 # Initialize the state vector to represent the desired initial state
 init = np.zeros(2**n, dtype=complex)
@@ -76,21 +96,23 @@ init[0] = 1.0
 magnetizationOperator = sum(unitaries.Z([i]).as_matrix(n) for i in range(n))
 
 # Time evolution data storage
-mag = []
+mag_PyQuEST = []
+mag_Quimb = []
 hMag = []
-spin = []
-hSpin = []
 
 # Trotterization
 eigvals_trotter, eigvecs_trotter = None, None # trotterization eigenvalues and eigenvectors
 for step in range(N):
     if step == 0:
-        trotter_step(n, reg, dt, J, h)
+        eigvals_trotter, eigvecs_trotter = trotter_step(n, [reg, circ], dt, J, h, r=step, periodic=False, getHam = True)
     else:
-        eigvals_trotter, eigvecs_trotter = trotter_step(n, reg, dt, J, h, periodic=False)
+        trotter_step(n, [reg, circ], dt, J, h, r=step, periodic=False, getHam = False)
 
-    state_vector = reg[:]
-    mag.append(np.vdot(state_vector, magnetizationOperator @ state_vector).real)
+    state_vector_PyQuEST = reg[:]
+    state_vector_Quimb = circ.psi.to_dense()
+    mag_PyQuEST.append(np.vdot(state_vector_PyQuEST, magnetizationOperator @ state_vector_PyQuEST).real)
+    mag_Quimb.append(np.vdot(state_vector_Quimb, magnetizationOperator @ state_vector_Quimb).real)
+
 
 # Initialize the Hamiltonian as a zero matrix
 dim = 2 ** n
@@ -137,65 +159,62 @@ for t in np.linspace(0, T, N - 1):
 for state in state_evolution[1:]:
     hMag.append(np.vdot(state, magnetizationOperator @ state).real)
 
-# Quimb hamiltonian simulation
+# Hamiltonian simulation
 binary = '0'*n
 
 # Bitstring -> MPS
 psi0 = qtn.MPS_computational_state(binary)
 
 ZZ = pauli('Z') & pauli('Z')
+
 H_mpo = qtn.LocalHam1D(n, H2=J*ZZ, H1=h*pauli('X'), cyclic=False)
 
 # Setting up TEBD object
-tebd = qtn.TEBD(psi0, H_mpo)
+tebd = qtn.TEBD(psi0, H_mpo, dt=dt)
 tebd.split_opts['cutoff'] = 1e-10
 
 # Observables
-mz_t_j = []  # z-magnetization
+zMags = []
 # Sampling TEBD at times
-for psit in tebd.at_times(np.linspace(0,T,N), tol=1e-3):
-    # Local observables
-    mz_j = []
-    info = {"cur_orthog": None}  # Current orthogonality center
-    # Adding current datapoints using orthogonality center
-    for j in range(1,n):
-        mz_j.append(psit.magnetization(j, info=info))
-    mz_t_j.append(mz_j)
+for psit in tebd.at_times(np.linspace(0,T,N)):
+    psi_t = psit
+    zMag = 0
+    for j in range(0,n):
+        zMag += psi_t.gate(Z, j).H @ psi_t
+    zMags.append(np.real(zMag))
 
 # Generate x-axis values as timesteps from 0 to T
 x_data = np.linspace(0, T, N)
 
 # Recreate the figure from scratch
-fig, ax = plt.subplots(4, 1, figsize=(9, 6), sharex=True, clear=True)
+fig, ax = plt.subplots(3, 1, figsize=(9, 6), sharex=True, clear=True)
 
 # Plotting as before
 x_data = np.linspace(0, T, N)
-ax[0].scatter(x_data, mag, label="Trotter magnetization (pyQuEST)", color="tab:blue", marker="x")
-ax[0].plot(x_data[:-1], hMag, label="Exact magnetization", color="tab:green")
-
-# Calculate the scaling factor based on the first point
-mz_t_j_sum = []
-for m in mz_t_j:
-    mz_t_j_sum.append(np.sum(m))
-
-scaling_factor = mz_t_j_sum[0] / hMag[0]
-
-# Rescale hMag using the scaling factor
-hMag_rescaled = [value * scaling_factor for value in hMag]
+ax[0].plot(x_data[:-1], hMag, label="Exact magnetization", color="tab:blue")
+ax[0].scatter(x_data, mag_PyQuEST, label="Trotter magnetization (pyQuEST)", color="tab:blue", s=5,)
+ax[0].scatter(x_data, mag_Quimb, label="Trotter magnetization (Quimb circuit)", color="tab:red", s=5)
 
 # Plot the rescaled data
-ax[1].scatter(x_data[:-1], mz_t_j_sum[:-1], marker="x", color="tab:blue", label="Trotter magnetization (Quimb)")
-ax[1].plot(x_data[:-1], hMag_rescaled, color="tab:green", label="Exact magnetization *SCALED")
+ax[1].plot(x_data[:-1], hMag, color="tab:red", label="Exact magnetization")
+ax[1].scatter(x_data[:-1], zMags[1:], s=5, color="tab:red", label="Trotter magnetization (Quimb TEBD)")
 
-magResidual = [[],[]]
+magResidual = [[],[],[]]
+mag_Quimb_TEBD = zMags[1:]
 for i in range(len(hMag)):
-    magResidual[0].append(mag[i]-hMag[i])
-    magResidual[1].append(hMag_rescaled[i]-mz_t_j_sum[i])
+    magResidual[0].append(mag_PyQuEST[i]-hMag[i])
+    magResidual[1].append(mag_Quimb[i]-hMag[i])
+    magResidual[2].append(mag_Quimb_TEBD[i]-hMag[i])
 
-ax[2].plot(x_data[:-1], magResidual[0], label="pyQuEST residual")
-ax[3].plot(x_data[:-1], magResidual[1], label="Quimb residual")
+ax[2].plot(x_data[:-1], magResidual[0], label="pyQuEST residual", color="tab:blue")
+ax[2].plot(x_data[:-1], magResidual[1], label="Quimb circuit residual", color="tab:blue")
+ax[2].plot(x_data[:-1], magResidual[2], label="Quimb TEBD residual", color="tab:red")
 
-for i in range(4):
+print(np.max(magResidual[1]))
+print(np.max(magResidual[2]))
+print(np.max(magResidual[2])/np.max(magResidual[1]))
+
+for i in range(3):
     ax[i].legend()
 plt.suptitle("Comparison of trotterization vs exact exponentiated Hamiltonian", fontsize=14)
 ax[0].set_title(f'n: {n} N: {N} T: {T}', fontsize=10)
