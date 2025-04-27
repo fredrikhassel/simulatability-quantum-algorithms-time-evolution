3d6a18f62770e5d7746c7264a0d3e4832eab362d
Fredrik <fredrik.hasselgren02@gmail.com>
2025-04-27 21:17:10 +0100

    setting up for cluster TEPAI calculations

--- Practice/te_pai-main/te_pai/__init__.py
+++
@@ -1,6 +0,0 @@
-from .hamil import Hamiltonian
-from .trotter import Trotter
-from .te_pai import TE_PAI
-from .sampling import resample
-import numpy as np
-print("DEBUG: Local 'te_pai' package loaded!")
