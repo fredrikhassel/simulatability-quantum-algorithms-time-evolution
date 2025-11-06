936a962a2b02fd4560db473ff2c3ac55752edb66
Fredrik <fredrik.hasselgren02@gmail.com>
2025-11-06 17:27:09 +0000

    setting up for q=25 p=1000 2D lattice circuit gen

--- TE-PAI-noSampling/SAMPLING.py
+++ TE-PAI-noSampling/SAMPLING.py
@@ -5,8 +5,10 @@ from scipy.stats import binom
 
 """This module contains the sampling functions used in the TE-PAI paper."""
 
-def batch_sampling(probs, batch_size):
-    return mp.Pool(mp.cpu_count()-2).map(sample_from_prob, [probs] * batch_size)
+def batch_sampling(probs, batch_size, n_workers=None):
+    if n_workers is not None:
+        return mp.Pool(n_workers).map(sample_from_prob, [probs] * batch_size) 
+    return mp.Pool(mp.cpu_count()-1).map(sample_from_prob, [probs] * batch_size)
 
 @jit(nopython=True)
 def custom_random_choice(prob):
